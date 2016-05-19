dd6b73608235a3154e70e9300db1c937997f46ff
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-05-19 08:38:21 +0100

    Add file size restriction to just 10MB

--- app/models/doc_attachment.rb
+++ app/models/doc_attachment.rb
@@ -4,7 +4,9 @@ class DocAttachment < ActiveRecord::Base
   has_attached_file :attachment
 
   validates_attachment_presence :attachment
-  validates_attachment_content_type :attachment, content_type: %w(application/pdf application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document text/plain)
+  validates_attachment_content_type :attachment,
+                                    content_type: %w(application/pdf application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document text/plain),
+                                    size: { :in => 0..10.megabytes }
 
   validates :doc_attachment_type_id, presence: true
   validates :code, presence: true
