962cdaf5b203aee00bdca52dfead7c263564f9a6
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-05-19 07:14:32 +0100

    Add code and title with validations and update the form

--- app/models/doc_attachment.rb
+++ app/models/doc_attachment.rb
@@ -3,5 +3,10 @@ class DocAttachment < ActiveRecord::Base
 
   has_attached_file :attachment
 
-  validates_attachment_content_type :attachment, content_type: /\Aimage/
+  validates_attachment_presence :attachment
+  validates_attachment_content_type :attachment, content_type: %w(application/pdf application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document text/plain)
+
+  validates :doc_attachment_type_id, presence: true
+  validates :code, presence: true
+  validates :title, presence: true
 end
--- spec/models/doc_attachment_spec.rb
+++ spec/models/doc_attachment_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe DocAttachment, type: :model do
 
   it { should belong_to(:doc_attachment_type) }
 
+  describe 'validations' do
+    it { should validate_presence_of(:doc_attachment_type_id)}
+    it { should validate_presence_of(:code) }
+    it { should validate_presence_of(:title) }
+
+  end
 end
