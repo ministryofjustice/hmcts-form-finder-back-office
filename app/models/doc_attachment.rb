27c486073e6512a88903ed4683939cd45d96c3df
Alistair Laing <Alistair.Laing@Digital.Justice.gov.uk>
2016-05-24 19:16:47 +0100

    Rename the uploaded files to code-language.extension

--- app/models/doc_attachment.rb
+++ app/models/doc_attachment.rb
@@ -2,11 +2,14 @@ class DocAttachment < ActiveRecord::Base
   belongs_to :doc_attachment_type
   belongs_to :language
 
-  has_attached_file :attachment
-
   acts_as_gov_uk_date :published_date
 
+  has_attached_file :attachment
+
+  before_save :rename_file
+
   validates_attachment_presence :attachment
+
   validates_attachment_content_type :attachment,
                                     content_type: %w(application/pdf application/vnd.ms-excel application/vnd.openxmlformats-officedocument.spreadsheetml.sheet application/msword application/vnd.openxmlformats-officedocument.wordprocessingml.document text/plain),
                                     size: { :in => 0..10.megabytes }
@@ -15,4 +18,13 @@ class DocAttachment < ActiveRecord::Base
   validates :code, presence: true
   validates :title, presence: true
   validates :language_id, presence: true
+
+  def rename_file
+    extension = File.extname(attachment_file_name).gsub(/^\.+/, '')
+    filename = attachment_file_name.gsub(/\.#{extension}$/, '')
+    new_attachment_file_name = "#{self.code}-#{self.language.name}.#{extension}"
+
+    attachment.instance_write(:file_name, new_attachment_file_name)
+
+  end
 end
