3bcb87ec152ef0b5630374580179388ad7c10ba9
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2016-11-01 15:41:28 +0000

    Added Categories from scaffold

---
+++ app/views/categories/_category.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! category, :id, :english_name, :welsh_name, :created_at, :updated_at
+json.url category_url(category, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160526081350) do
+ActiveRecord::Schema.define(version: 20161101085825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "english_name"
+    t.string   "welsh_name"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "doc_attachment_types", force: :cascade do |t|
     t.string   "name",       null: false
