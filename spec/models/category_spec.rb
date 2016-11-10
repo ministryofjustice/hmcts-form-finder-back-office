d652dfc07675acac460845c317dd9a20de9403be
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2016-11-10 13:27:23 +0000

    Validate category for a name - either in Welsh or English, or both.

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(Category.create(english_name: "George", welsh_name: "David") ).to be_valid
+  end
+  it "can have an English name if there is no Welsh name" do
+    expect(Category.create(english_name: "George") ).to be_valid
+  end
+  it "can have an Welsh name if there is no Welsh name" do
+    expect(Category.create(welsh_name: "David") ).to be_valid
+  end
+  it "must have an English name if there is no Welsh name" do
+    expect(Category.create ).to_not be_valid
+  end
+  it "must have an Welsh name if there is no English name" do
+    expect(Category.create ).to_not be_valid
+  end
 end
