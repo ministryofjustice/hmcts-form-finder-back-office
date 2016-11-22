faf9f5dfa4878efbb48ad9609a90d6f9db16ac7b
Nick Preddy <nick.preddy@digital.justice.gov.uk>
2016-11-22 11:26:27 +0000

    Category table seeding
    Inserting 72 document categories into the database.  Also temporarily
    commented out the forms and leaflets seeding functions.  There is no
    need yet to download all the document files

--- db/migrate/20161101085825_create_categories.rb
+++ db/migrate/20161101085825_create_categories.rb
@@ -3,7 +3,6 @@ class CreateCategories < ActiveRecord::Migration
     create_table :categories do |t|
       t.string :english_name
       t.string :welsh_name
-
       t.timestamps null: false
     end
   end
