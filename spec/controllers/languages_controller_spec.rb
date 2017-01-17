b992379515a80e1225de7ffdd3d3c4d21c0d8a9d
Rohan Salunkhe <rohan.salunkhe@digital.justice.gov.uk>
2017-01-17 10:36:49 +0000

    Pending tests

--- spec/controllers/languages_controller_spec.rb
+++ spec/controllers/languages_controller_spec.rb
@@ -24,11 +24,11 @@ RSpec.describe LanguagesController, type: :controller do
   # Language. As you add validations to Language, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    attributes_for(:language)
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {english_name:nil, welsh_name:nil, code:nil}
   }
 
   # This should return the minimal set of values that should be in the session
@@ -104,7 +104,7 @@ RSpec.describe LanguagesController, type: :controller do
   describe "PUT #update" do
     context "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        attributes_for(:language)
       }
 
       it "updates the requested language" do
