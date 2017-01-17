27a6efbd035fbf311065c9981c74f6b9ac9bcdaa
Rohan Salunkhe <rohan.salunkhe@digital.justice.gov.uk>
2017-01-17 10:36:49 +0000

    Pending tests

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :languages
   resources :languages
   resources :languages
-  devise_for :users
+  devise_for :users, :path_prefix => 'my'git
   resources :users
 
   authenticated :user  do
