cc91f2d2e4a5a07eaf24cee57a8e8bfcf1e2755a
Rohan Salunkhe <rohan.salunkhe@digital.justice.gov.uk>
2017-01-31 16:01:40 +0000

    Language db create conflict resolved

--- db/migrate/20161220144036_create_languages.rb
+++ db/migrate/20161220144036_create_languages.rb
@@ -3,11 +3,7 @@ class CreateLanguages < ActiveRecord::Migration
     create_table :languages do |t|
       t.string :english_name
       t.string :welsh_name
-<<<<<<< c840566d3bd171f0a39ac707c3bb344089b8a647:db/migrate/20161220144036_create_languages.rb
       t.string :code, null:false
-=======
-      t.string :code
->>>>>>> Language CRUD functions added:db/migrate/20161220144036_create_languages.rb
 
       t.timestamps null: false
     end
