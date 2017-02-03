dce2d2d58ae1741b0960c20003bb9758cd3be95c
Andrew Larssen <andrew.larssen@digital.justice.gov.uk>
2017-02-03 10:19:40 +0000

    Add annotate gem
    Add annotate gem and annotate models

--- app/models/doc_attachment_type.rb
+++ app/models/doc_attachment_type.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: doc_attachment_types
+#
+#  id           :integer          not null, primary key
+#  english_name :string
+#  welsh_name   :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+
 class DocAttachmentType < ActiveRecord::Base
   validates :english_name, presence: true, unless: :welsh_name
   validates :welsh_name, presence: true, unless: :english_name
