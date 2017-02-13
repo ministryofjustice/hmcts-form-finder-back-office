419c53a48e3e96b13553dcbf76782882260d5e5e
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2017-02-13 15:37:18 +0000

    Fixed tests to use new routing for I18n

--- spec/views/users/show.html.haml_spec.rb
+++ spec/views/users/show.html.haml_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "users/show", type: :view do
   before(:each) do
+
     @user = assign(:user, User.create!(
       :first_name => "First Name",
       :last_name => "Last Name",
