067b0682f91436fd4fe4756684e61a408e0913ba
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2017-02-15 16:18:13 +0000

    Test for both english and welsh

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
-  
+
   # SSL Production Config
   if Rails.env.production? && !HostEnv.is_dev?
     # Force SSL except in excepted routes
