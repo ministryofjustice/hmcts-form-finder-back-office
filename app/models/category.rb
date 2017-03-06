2b2b00c30bd4df17fb67a4f73bbd93747b7774eb
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2017-03-06 15:54:16 +0000

    Rename validators - Jenkins was failing

--- app/models/category.rb
+++ app/models/category.rb
@@ -8,7 +8,7 @@
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #
-require 'Validators'
+require 'validators'
 class Category < ActiveRecord::Base
 
   extend  SoftDeletion::Collection
