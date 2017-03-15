6c95e981884105d0aace4fe08a0c527c20dcd42f
Nick Preddy <nick.preddy@digital.justice.gov.uk>
2017-03-15 15:21:03 +0000

    Document table amendments
    Removed 'category' string column fro table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170301090437) do
+ActiveRecord::Schema.define(version: 20170315145821) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,7 +46,6 @@ ActiveRecord::Schema.define(version: 20170301090437) do
   create_table "documents", force: :cascade do |t|
     t.string   "code"
     t.string   "title"
-    t.string   "category"
     t.integer  "doc_attachment_type_id",                  null: false
     t.datetime "created_at",                              null: false
     t.datetime "updated_at",                              null: false
