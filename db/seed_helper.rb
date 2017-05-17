d559ec634e40f29080ec8bb4ce8dab21cfe96b21
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2017-05-17 12:53:03 +0100

    Use a guard clause instead of wrapping the code inside a conditional expression.
    Conform to Ruby style

--- db/seed_helper.rb
+++ db/seed_helper.rb
@@ -1,7 +1,7 @@
 module SeedHelper
   def self.find_or_create_user!(attrs)
     user = User.find_by(first_name: attrs[:first_name], last_name: attrs[:last_name], email: attrs[:email].downcase)
-    if user.blank?
+    return unless user.blank?
       User.create!(
         first_name: attrs[:first_name],
         last_name:  attrs[:last_name],
@@ -9,6 +9,5 @@ module SeedHelper
         password:   ENV.fetch(attrs[:password_env_var]),
         password_confirmation: ENV[attrs[:password_env_var]]
       )
-    end
   end
 end
