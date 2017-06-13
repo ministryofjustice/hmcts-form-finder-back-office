fd3586aee7087be50d441447516f7ca5eafb9fde
Nick Preddy <nick.preddy@digital.justice.gov.uk>
2017-06-13 15:21:53 +0100

    Rationalise language controller
    Moved repeating respond_to actions to a single private function.

--- app/controllers/languages_controller.rb
+++ app/controllers/languages_controller.rb
@@ -19,33 +19,18 @@ class LanguagesController < ApplicationController
 
   def create
     @language = Language.new(language_params)
-
-    respond_to do |format|
-      if @language.save
-        format.html { redirect_to @language, notice: t('language.create_success') }
-      else
-        format.html { render :new }
-      end
-    end
+    apply_change("add")
   end
 
   def update
-    respond_to do |format|
-      if @language.update(language_params)
-        format.html { redirect_to @language, notice: t('language.update_success') }
-      else
-        format.html { render :edit }
-      end
-    end
+    apply_change("edit")
   end
 
   # DELETE /languages/1
   # DELETE /languages/1.json
   def destroy
     @language.destroy
-    respond_to do |format|
-      format.html { redirect_to languages_url, notice: t('language.delete_success') }
-    end
+    apply_change("delete")
   end
 
   private
@@ -59,4 +44,26 @@ class LanguagesController < ApplicationController
   def language_params
     params.require(:language).permit(:english_name, :welsh_name, :code, :inactive)
   end
+
+  def apply_change(action)
+    respond_to do |format|
+      case action
+      when "add"
+        if @language.save
+          format.html { redirect_to @language, notice: t('language.create_success') }
+        else
+          format.html { render :new }
+        end
+      when "edit"
+        if @language.update(language_params)
+          format.html { redirect_to @language, notice: t('language.update_success') }
+        else
+          format.html { render :edit }
+        end
+      else
+        format.html { redirect_to languages_url, notice: t('language.delete_success') }
+      end
+    end
+  end
+
 end
