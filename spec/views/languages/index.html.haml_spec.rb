faeb270389bbb8746073cc1dd2ffccc41dc83c57
Rohan Salunkhe <rohan.salunkhe@digital.justice.gov.uk>
2017-08-15 16:15:30 +0100

    language active/inactive filters applied.

--- spec/views/languages/index.html.haml_spec.rb
+++ spec/views/languages/index.html.haml_spec.rb
@@ -2,7 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'languages/index', type: :view do
   before(:each) do
-    assign(:languages, [
+    assign(:active_languages, [
+             Language.create!(
+               english_name: 'English Name',
+               welsh_name: 'Welsh Name',
+               code: 'Code'
+             ),
+             Language.create!(
+               english_name: 'English Name',
+               welsh_name: 'Welsh Name',
+               code: 'Code'
+             )
+           ])
+    assign(:inactive_languages, [
              Language.create!(
                english_name: 'English Name',
                welsh_name: 'Welsh Name',
@@ -18,8 +30,8 @@ RSpec.describe 'languages/index', type: :view do
 
   it 'renders a list of languages' do
     render
-    assert_select 'tr>td', text: 'English Name'.to_s, count: 2
-    assert_select 'tr>td', text: 'Welsh Name'.to_s, count: 2
-    assert_select 'tr>td', text: 'Code'.to_s, count: 2
+    assert_select 'tr>td', text: 'English Name'.to_s, count: 4
+    assert_select 'tr>td', text: 'Welsh Name'.to_s, count: 4
+    assert_select 'tr>td', text: 'Code'.to_s, count: 4
   end
 end
