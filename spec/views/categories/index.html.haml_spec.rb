ab3d0981560127dda92c0d038c23a862fb6a87dc
Rohan Salunkhe <rohan.salunkhe@digital.justice.gov.uk>
2017-08-15 15:41:46 +0100

    Specs fixed for active/inactive categories

--- spec/views/categories/index.html.haml_spec.rb
+++ spec/views/categories/index.html.haml_spec.rb
@@ -1,7 +1,17 @@
 require 'rails_helper'
 RSpec.describe 'categories/index', type: :view do
   before(:each) do
-    assign(:categories, [
+    assign(:active_categories, [
+             Category.create!(
+               english_name: 'English Name',
+               welsh_name: 'Welsh Name'
+             ),
+             Category.create!(
+               english_name: 'English Name',
+               welsh_name: 'Welsh Name'
+             )
+           ])
+    assign(:inactive_categories, [
              Category.create!(
                english_name: 'English Name',
                welsh_name: 'Welsh Name'
@@ -15,7 +25,7 @@ RSpec.describe 'categories/index', type: :view do
 
   it 'renders a list of categories' do
     render
-    assert_select 'tr>td', text: 'English Name'.to_s, count: 2
-    assert_select 'tr>td', text: 'Welsh Name'.to_s, count: 2
+    assert_select 'tr>td', text: 'English Name'.to_s, count: 4
+    assert_select 'tr>td', text: 'Welsh Name'.to_s, count: 4
   end
 end
