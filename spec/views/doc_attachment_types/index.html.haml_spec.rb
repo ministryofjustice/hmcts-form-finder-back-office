514f6a64c1bce0c96543d47c268a6e18ca3626df
Rohan Salunkhe <rohan.salunkhe@digital.justice.gov.uk>
2017-08-24 15:38:20 +0100

    fixed spec for document types

--- spec/views/doc_attachment_types/index.html.haml_spec.rb
+++ spec/views/doc_attachment_types/index.html.haml_spec.rb
@@ -2,7 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'doc_attachment_types/index', type: :view do
   before(:each) do
-    assign(:doc_attachment_types, [
+    assign(:active_doc_attachment_types, [
+             DocAttachmentType.create!(
+               english_name: 'English Name',
+               welsh_name: 'Welsh Name'
+             ),
+             DocAttachmentType.create!(
+               english_name: 'English Name',
+               welsh_name: 'Welsh Name'
+             )
+           ])
+    assign(:inactive_doc_attachment_types, [
              DocAttachmentType.create!(
                english_name: 'English Name',
                welsh_name: 'Welsh Name'
@@ -16,7 +26,7 @@ RSpec.describe 'doc_attachment_types/index', type: :view do
 
   it 'renders a list of doc_attachment_types' do
     render
-    assert_select 'tr>td', text: 'English Name'.to_s, count: 2
-    assert_select 'tr>td', text: 'Welsh Name'.to_s, count: 2
+    assert_select 'tr>td', text: 'English Name'.to_s, count: 4
+    assert_select 'tr>td', text: 'Welsh Name'.to_s, count: 4
   end
 end
