70c67fb766d53595f5739cb0b5796490154a76ad
Rohan Salunkhe <rohan.salunkhe@digital.justice.gov.uk>
2017-11-13 15:03:06 +0000

    DEPRECATION WARNING removed

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,6 @@ module HmctsFormFinderBackOffice
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
 
     ActionView::Base.default_form_builder = GovukElementsFormBuilder::FormBuilder
   end
