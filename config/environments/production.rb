a581b28621f9c6d404b308a42994aa72ef128c2c
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2017-11-14 14:18:22 +0000

    Default to localhost
    For Travis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
 
   # Don't care if the mailer can't send.
-  config.action_mailer.default_url_options = { host: ENV.fetch('SENDING_HOST'), port: 3000 }
+  config.action_mailer.default_url_options = { host: ENV.fetch('SENDING_HOST', 'localhost'), port: 3000 }
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
