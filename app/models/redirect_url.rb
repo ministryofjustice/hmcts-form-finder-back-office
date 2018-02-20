22618f1496dd6b0f179a1682fc18545781575751
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2018-02-20 15:59:11 +0000

    Create a model for redirection
    Both Document (details) and Categories need redirects

---
+++ app/models/redirect_url.rb
@@ -0,0 +1,2 @@
+class RedirectUrl < ApplicationRecord
+end
---
+++ db/migrate/20180220155818_create_redirect_urls.rb
@@ -0,0 +1,12 @@
+class CreateRedirectUrls < ActiveRecord::Migration[5.0]
+  def change
+    create_table :redirect_urls do |t|
+      t.string :incoming
+      t.string :outgoing
+      t.integer :document_id
+      t.integer :category_id
+
+      t.timestamps
+    end
+  end
+end
