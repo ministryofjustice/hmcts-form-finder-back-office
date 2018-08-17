9bf4bad3a3cc6529df3b86e315fedd56ac56417d
Nick Preddy <nick.preddy@digital.justice.gov.uk>
2018-08-17 16:00:05 +0100

    Rubocop-rspec gem installed.
    Static attributes will be removed in FactoryBot 5.0. Rubocop-rspec gem installed to wrap the attribute values in a block.

--- spec/factories/redirect_urls.rb
+++ spec/factories/redirect_urls.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :redirect_url do
-    incoming 'MyString'
-    outgoing 'MyString'
-    inactive false
+    incoming { 'MyString' }
+    outgoing { 'MyString' }
+    inactive { false }
   end
 end
